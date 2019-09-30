26434dd8a9c7e5f75febb5ea8fec8df758081730
qudlaty <48454878+qudlaty@users.noreply.github.com>
2019-09-30 22:45:16 +0200

    Add repo files

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+//Check off tasks
+
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+  console.log('clik');
+  
+});
+
+// click on X to del task
+
+$("ul").on("click","span", function(e){
+  $(this).parent().fadeOut(600,function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
+// add new task
+
+$("input[type='text']").keydown(function(e){
+  if(e.which === 13){
+    var taskTxt = $(this).val();
+    $(this).val('');
+    $("ul").append('<li><span><i class="far fa-times-circle"></i></span> ' + taskTxt + '</li>'); 
+  }
+});
+
+$("h1 span").click(function(){
+  $("input[type='text']").fadeToggle();
+});
