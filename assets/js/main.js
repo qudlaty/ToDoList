ac9f0f24fa465575c02b6ec1a75403ea4a8aa6e2
qudlaty <qudlaty@users.noreply.github.com>
2021-09-24 19:10:00 +0200

    Addind and removing from localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,36 @@
-
-var existing = localStorage.getItem('listItems');
-console.log( existing);
-
+// add loading tasks from local storage on start
+// populateList() =>{}
 
 //Check off tasks
 $("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
-  console.log('clicked on li');
 });
 
-// click on X to del task
-
+// click on X to del task adn remove from localStorage
 $("ul").on("click","span", function(e){
-					
-  $(this).parent().fadeOut(600,function(){
-				
-		localStorage.removeItem('listItems');
 
+  $(this).parent().fadeOut(600,function(){
+  
+    var taskTxt = $(this).text();
+    console.log(String.taskTxt,'===', taskTxt);
+    localStorage.removeItem(taskTxt);
   });
+
   e.stopPropagation();
 });
 
-// add new task
-
+// add new task to page and localStorage
 $("input[type='text']").keydown(function(e){
   if(e.which === 13 && $(this).val() !== ''){
     var taskTxt = $(this).val();
     $(this).val('');
     $("ul").append('<li><span><i class="far fa-times-circle"></i></span> ' + taskTxt + '</li>'); 
-		if(!existing){existing = []};
 
- 		existing.push(taskTxt);
-		localStorage.setItem('listItems', existing);
-		
-		console.log(window.localStorage, existing);
-		
+    localStorage.setItem(' '+taskTxt,taskTxt);
   }
 });
 
-console.log('local storage content', window.localStorage, existing)
+//console.log('local storage content', window.localStorage)
 
 $("h1 span").click(function(){
   $("input[type='text']").fadeToggle();
